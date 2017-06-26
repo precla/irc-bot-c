bd4d7150e79520924cc778003fd35e6b32fdca33
precla <antonio.prcela@gmail.com>
2017-06-26 19:48:52 +0200

    print event as unsigned int as it is

--- irc-bot-c/events.cpp
+++ irc-bot-c/events.cpp
@@ -13,7 +13,7 @@ void event_connect(irc_session_t * session, const char * event, const char * ori
 
 void event_numeric(irc_session_t * session, unsigned int event, const char * origin, const char ** params, unsigned int count) {
     if (event > 400) {
-        printf("ERROR %d: %s: %s %s %s %s\n",
+        printf("ERROR %u: %s: %s %s %s %s\n",
             event,
             origin ? origin : "unknown",
             params[0],
